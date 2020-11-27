588d766ef30b6c5203f5a5d5026cf83dee7a5295
rmfpdlxmtidl <gwak2837@kakao.com>
2020-11-28 00:18:56 +0900

    Add sample files

---
+++ src/components/Hello2.tsx
@@ -0,0 +1,24 @@
+import { useCallback, useState } from 'react'
+
+type Props = {
+  a: number
+  b: string
+}
+
+function Hello2({ a, b }: Props) {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClick = useCallback(() => {
+    setIsLoading(!isLoading)
+  }, [isLoading])
+
+  return (
+    <>
+      <div>{a}</div>
+      <div>{b}</div>
+      <div onClick={handleClick}>{isLoading}</div>
+    </>
+  )
+}
+
+export default Hello2
