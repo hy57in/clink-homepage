dd7c438098ab00c8954b5023ef31fcd085d6ca96
곽태욱 <gwak2837@kakao.com>
2021-01-10 19:24:52 +0900

    Add description and keywords meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,11 +13,16 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
+const description = '안녕하세요. 중앙대학교 블록체인 학회 C-Link입니다.'
+const keywords = 'C-Link, 블록체인, 중앙대, 중앙대학교, 학회, 동아리'
+
 function CLinkApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width, minimal-ui" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <GlobalStyle />
