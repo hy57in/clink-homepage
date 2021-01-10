bc493dfb962201be7b2815219c8422c4c8baec68
곽태욱 <gwak2837@kakao.com>
2021-01-10 17:58:54 +0900

    Create ImageBox.tsx

---
+++ src/components/atoms/ImageBox.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react'
+import styled from 'styled-components'
+
+const FlexContainer = styled.div<{ rightImage: boolean }>`
+  display: flex;
+  flex-flow: ${(p) => (p.rightImage ? 'row-reverse' : 'row')} nowrap;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`
+
+const FlexContainerColumn = styled.div<{ rightImage: boolean }>`
+  display: flex;
+  flex-flow: column nowrap;
+
+  & :last-child {
+    text-align: ${(p) => (p.rightImage ? 'right' : 'left')};
+  }
+`
+
+type Props = {
+  children: ReactNode[]
+  rightImage?: boolean
+}
+
+function ImageBox({ children, rightImage = false }: Props) {
+  return (
+    <FlexContainer rightImage={rightImage}>
+      {children[0]}
+      <FlexContainerColumn rightImage={rightImage}>
+        {children[1]}
+        {children[2]}
+      </FlexContainerColumn>
+    </FlexContainer>
+  )
+}
+
+export default ImageBox
