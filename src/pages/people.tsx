1ff9e2be08168647ad510e77f6aebcc3c79f7f70
DongjaeJang <jangdj001234@gmail.com>
2021-01-22 21:33:04 +0900

    Modify css style according to feedback

--- src/pages/people.tsx
+++ src/pages/people.tsx
@@ -1,57 +1,83 @@
 import styled from 'styled-components'
+import Link from 'next/link'
 
 const data = [
   {
+    id: 0,
     name: 'JAMES',
     info1: 'Leader',
     info2: 'Research',
     info3: 'Education',
     image: 'https://yceffort.kr/2020/10/profile.png',
     address: 'https://www.facebook.com/CAUCLink',
+    github: '',
+    facebook: '',
+    notion: '',
   },
   {
+    id: 1,
     name: 'MICHAEL',
     info1: 'Leader',
     info2: 'Research',
     info3: 'Education',
     image: './person1.png',
     address: 'https://www.naver.com',
+    github: '',
+    facebook: '',
+    notion: '',
   },
   {
+    id: 2,
     name: 'LILLY',
     info1: 'Leader',
     info2: 'Research',
     info3: 'Education',
     image: 'https://yceffort.kr/2020/10/profile.png',
     address: 'https://www.naver.com',
+    github: '',
+    facebook: '',
+    notion: '',
   },
   {
+    id: 3,
     name: 'JESSICA',
     info1: 'Leader',
     info2: 'Research',
     info3: 'Education',
     image: './person1.png',
     address: 'https://www.naver.com',
+    github: '',
+    facebook: '',
+    notion: '',
   },
   {
+    id: 4,
     name: 'PETER',
     info1: 'Leader',
     info2: 'Research',
     info3: 'Education',
     image: './person1.png',
     address: 'https://www.naver.com',
+    github: '',
+    facebook: '',
+    notion: '',
   },
   {
+    id: 5,
     name: 'JENNY',
     info1: 'Leader',
     info2: 'Research',
     info3: 'Education',
     image: './person1.png',
     address: 'https://www.naver.com',
+    github: '',
+    facebook: '',
+    notion: '',
   },
 ]
 
 const BigContainer = styled.div`
+  margin: 0px auto;
   align-items: center;
   max-width: 900px;
   margin-top: 3rem;
@@ -66,6 +92,10 @@ const More = styled.a`
   &:hover {
     color: #0070f3;
   }
+`
+
+const MoreText = styled.h6`
+  text-align: right;
 `
 
 const GridContainer = styled.div`
@@ -99,7 +129,7 @@ const PersonalBox = styled.div`
 const ImageBox = styled.div`
   width: 200px;
   height: 200px;
-  border-radius: 70%;
+  border-radius: 50%;
   overflow: hidden;
   background-color: lightblue;
 `
@@ -126,12 +156,14 @@ const BlockInfo = styled.h5`
 function People() {
   return (
     <BigContainer>
-      <h6 style={{ textAlign: 'right' }}>
-        <More href="/team">MORE &rarr;</More>
-      </h6>
+      <MoreText>
+        <Link href="/team">
+          <More>MORE &rarr;</More>
+        </Link>
+      </MoreText>
       <GridContainer>
-        {data.map((person, i) => (
-          <a href={person.address} target="_blank" key={i} rel="noreferrer">
+        {data.map((person) => (
+          <Link href={person.address} key={person.id}>
             <PersonalBox>
               <ImageBox>
                 <Image src={person.image} />
@@ -141,7 +173,7 @@ function People() {
               <BlockInfo>{person.info2}</BlockInfo>
               <BlockInfo>{person.info3}</BlockInfo>
             </PersonalBox>
-          </a>
+          </Link>
         ))}
         )
       </GridContainer>
