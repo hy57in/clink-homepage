897ca065ba9623ff68e74b4f3a2cfa8146061c9f
DongjaeJang <jangdj001234@gmail.com>
2021-01-26 21:05:05 +0900

    fix map error

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -208,7 +208,8 @@ function TeamPage() {
                             </StyledA>
                           </Link>
                         )
-                    } else return
+                      else return ''
+                    }
                   })}
                 </FlatformContainer>
               </InfoContainer>
